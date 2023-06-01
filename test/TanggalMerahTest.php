3a5c736c8585663b9e96041492bd0540ebe08ccb
Am K <myawn@pm.me>
2023-06-01 12:10:33 +0700

    update

--- test/TanggalMerahTest.php
+++ test/TanggalMerahTest.php
@@ -1,13 +1,14 @@
-<?php
+<?php declare(strict_types=1);
+
 use PHPUnit\Framework\TestCase;
 final class TanggalMerahTest extends TestCase
 {
     private $pt;
-    protected function setUp()
+    public static function setUpBeforeClass(): void
     {
         $this->pt = new \Grei\TanggalMerah();
     }
-    protected function tearDown()
+    public static function tearDownAfterClass(): void
     {
         $this->pt = null;
     }
